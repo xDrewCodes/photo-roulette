b36618508066f220e7cab3ede49cfb8d6d6fd60c
Drew Allred <drew.e.allred@gmail.com>
2025-03-26 11:46:01 -0400

    build cleanup and prep

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = ({ user }) => {
     return (
         <section className="home center">
             <h1 className="home__title">Photo Roulette</h1>
-            <input className="name" placeholder={ user && user.displayName || 'Nickname'}></input>
+            <input className="name" placeholder={ (user && user.displayName) || 'Nickname'}></input>
             <div className="btn">Set Name</div>
             <div className="btn" onClick={() => navigate('/game')}>Join Game</div>
         </section>
